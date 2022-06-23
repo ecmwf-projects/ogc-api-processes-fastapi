36d998e43adad8e774546ca9e4c6c41d7e8baa42
Marco Cucchi <m.cucchi@bopen.eu>
2022-06-23 16:46:50 +0200

    init base client

--- ogc_api_processes_fastapi/api.py
+++ ogc_api_processes_fastapi/api.py
@@ -1,33 +1,22 @@
+import abc
 import typing as T
-import urllib.parse
 
-import fastapi
-
-PROCESS_LIST: list[dict[str, str]] = [
-    {"id": "retrieve-reanalysis-era5-single-levels", "version": "0.1"},
-    {"id": "retrieve-reanalysis-era5-pressure-levels", "version": "0.1"},
-    {"id": "retrieve-reanalysis-era5-land", "version": "0.1"},
-    {"id": "retrieve-reanalysis-era5-land-monthly-means", "version": "0.1"},
-]
+from . import models
 
 
-def get_processes_links(request: fastapi.Request) -> list[dict[str, T.Any]]:
+class BaseClient(abc.ABC):
     """
-    Return links associated to the processes list request.
+    Defines a pattern for implementing OGC API - Processes endpoints.
     """
-    retval = [
-        {
-            "href": urllib.parse.urljoin(str(request.base_url), "processes"),
-            "rel": "self",
-        }
-    ]
-    return retval
 
+    @abc.abstractmethod
+    def get_processes(self, **kwargs: T.Any) -> models.ProcessesList:
+        """
+        Get all available processes.
 
-def get_processes_list(
-    limit: int = fastapi.Query(default=10, ge=1, le=100)
-) -> list[dict[str, T.Any]]:
-    """
-    Return the list of processes.
-    """
-    return PROCESS_LIST[:limit]
+        Called with `GET /processes`.
+
+        Returns:
+            An object of type models.ProcessesList.
+        """
+        ...
