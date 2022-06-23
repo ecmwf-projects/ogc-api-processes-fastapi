0ff01e5b09885fcf52b1119370b6217426de068b
Marco Cucchi <m.cucchi@bopen.eu>
2022-06-23 18:19:50 +0200

    init api class

--- ogc_api_processes_fastapi/api/app.py
+++ ogc_api_processes_fastapi/api/app.py
@@ -1,1 +1,15 @@
 "fastapi app creation"
+
+import attr
+
+
+@attr.define()
+class ProcessingAPI:
+    """
+    ProcessingAPI factory.
+
+    Factory for creating an OGC FastAPI application.  After instantation, the application is accessible from
+    the `StacApi.app` attribute.
+    """
+
+    ...
