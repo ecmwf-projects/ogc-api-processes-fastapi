350e412a6afecf2c0901e545db14f38633d33b55
Marco Cucchi <m.cucchi@bopen.eu>
2022-06-24 14:55:44 +0200

    toy implementation of base client and instantiate_app()

--- ogc_api_processes_fastapi/main.py
+++ ogc_api_processes_fastapi/main.py
@@ -1,11 +1,56 @@
-from fastapi import FastAPI
+import abc
+from typing import Any
 
-from . import routers
+import fastapi
+
+from . import models
 
 
-def instantiate_app() -> FastAPI:
+class BaseClient(abc.ABC):
+    """
+    Defines a pattern for implementing OGC API - Processes endpoints.
+    """
 
-    app = FastAPI()
-    app.include_router(routers.processes_router)
+    @abc.abstractmethod
+    def get_processes(self, **kwargs: Any) -> dict[str, list[dict[str, Any]]]:
+        """
+        Get all available processes.
+
+        Called with `GET /processes`.
+
+        Returns:
+            An object of type models.ProcessesList.
+        """
+        ...
+
+
+def instantiate_app(client: BaseClient) -> fastapi.FastAPI:
+
+    app = fastapi.FastAPI()
+
+    processes_router = fastapi.APIRouter(
+        prefix="/processes",
+        tags=["Processes"],
+    )
+
+    @processes_router.get(
+        "/",
+        response_model=models.ProcessesList,
+        response_model_exclude_none=True,
+        summary="retrieve the list of available processes",
+        operation_id="geProcesses",
+    )
+    def get_processes_list(
+        request: fastapi.Request, limit: int = fastapi.Query(default=10, ge=1, le=100)
+    ) -> dict[str, list[dict[str, Any]]]:
+        """
+        The list of processes contains a summary of each process
+        the OGC API - Processes offers, including the link to a
+        more detailed description of the process.
+        """
+        process_list = client.get_processes(request=request)
+        return process_list
+
+    app.include_router(processes_router)
 
     return app
