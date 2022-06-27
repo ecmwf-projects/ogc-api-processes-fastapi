2c6d3c4ccc426eecb0f05c1f223f01daab5fb010
Marco Cucchi <m.cucchi@bopen.eu>
2022-06-27 12:07:54 +0200

    implement include ogc api processes routers

--- ogc_api_processes_fastapi/main.py
+++ ogc_api_processes_fastapi/main.py
@@ -3,20 +3,41 @@ import fastapi
 from . import clients, routers
 
 
-def instantiate_app(client: clients.BaseClient) -> fastapi.FastAPI:
+def instantiate_ogc_api_processes_app(client: clients.BaseClient) -> fastapi.FastAPI:
     """
-    Create an in instance of an OGC API - Processing compliant FastAPI application.
+    Create an instance of an OGC API - Processes compliant FastAPI application.
 
     Arguments:
         client:
-            A subclass of `BaseClient`. Defines the application logic which is injected into the API.
+            Defines the application logic which is injected into the API.
 
     Returns:
         app:
             The FastAPI application.
     """
-
     app = fastapi.FastAPI()
+    processes_router = routers.create_processes_router(client=client)
+    app.include_router(processes_router)
+
+    return app
+
+
+def include_ogc_api_processes_routers(
+    app: fastapi.FastAPI, client: clients.BaseClient
+) -> fastapi.FastAPI:
+    """
+    Add OGC API - Processes compliant routers to a FastAPI application.
+
+    Arguments:
+        app:
+            A FastAPI application.
+        client:
+            Defines the application logic which is injected into the API.
+
+    Returns:
+        app:
+            A FastAPI application with OGC API - Processes routes added.
+    """
     processes_router = routers.create_processes_router(client=client)
     app.include_router(processes_router)
 
