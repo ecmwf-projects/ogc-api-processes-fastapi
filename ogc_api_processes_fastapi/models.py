3cedd99f992f75eb616c9e62f1901931cf1f4d5a
Marco Cucchi <m.cucchi@bopen.eu>
2022-07-01 18:21:40 +0200

    change Process model inputs and outputs types

--- ogc_api_processes_fastapi/models.py
+++ ogc_api_processes_fastapi/models.py
@@ -145,5 +145,5 @@ class OutputDescription(DescriptionType):
 
 
 class Process(ProcessSummary):
-    inputs: Optional[InputDescription]
-    outputs: Optional[OutputDescription]
+    inputs: Optional[list[dict[str, InputDescription]]]
+    outputs: Optional[list[dict[str, OutputDescription]]]
