6e64a7fe382f3a1d456a61a2349ed25f0c76dcaa
Marco Cucchi <m.cucchi@bopen.eu>
2022-10-18 14:53:57 +0200

    add test

--- tests/test_20_api.py
+++ tests/test_20_api.py
@@ -28,3 +28,35 @@ def test_api_default(
     assert "/jobs" in routes_path
     assert "/jobs/{job_id}" in routes_path
     assert "/jobs/{job_id}/results" in routes_path
+
+    openapi_schema = app.openapi()
+    assert (
+        "metadata"
+        not in openapi_schema["components"]["schemas"]["StatusInfo"][
+            "properties"
+        ].keys()
+    )
+
+
+def test_api_add_resp_params(
+    test_client: ogc_api_processes_fastapi.BaseClient,
+) -> None:
+    add_resp_params = {"StatusInfo": {"metadata": (str, ...)}}
+    api = ogc_api_processes_fastapi.api.OGCProcessesAPI(
+        client=test_client, add_resp_params=add_resp_params
+    )
+    app = api.app
+    routes_path = [app.routes[i].path for i in range(len(app.routes))]
+
+    assert "/processes" in routes_path
+    assert "/processes/{process_id}" in routes_path
+    assert "/processes/{process_id}/execute" in routes_path
+    assert "/jobs" in routes_path
+    assert "/jobs/{job_id}" in routes_path
+    assert "/jobs/{job_id}/results" in routes_path
+
+    openapi_schema = app.openapi()
+    assert (
+        "metadata"
+        in openapi_schema["components"]["schemas"]["StatusInfo"]["properties"].keys()
+    )
